0cf81ed47275a3a0d7a95f15b8fa11f2a168ffeb
25VrushtiPatel <vrushti.padaliya@gmail.com>
2025-05-05 12:43:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todo/splashActivity.java
@@ -0,0 +1,30 @@
+package com.example.todo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = findViewById(R.id.splash_image);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
